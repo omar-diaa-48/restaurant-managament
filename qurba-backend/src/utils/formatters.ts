6056644bcb2fce2a3eda965cedea26ca732c79f9
Omar <omar@kamelizer.com>
2022-04-18 20:53:37 +0200

    adjust response formatter

---
+++ qurba-backend/src/utils/formatters.ts
@@ -0,0 +1,11 @@
+import { SuccessResponse } from "../types/responses"
+
+export const formatResponse = (data: any[], action: string, code: number, message?: string): SuccessResponse => {
+	return {
+		data,
+		count: data?.length,
+		action,
+		code,
+		message
+	}
+}
