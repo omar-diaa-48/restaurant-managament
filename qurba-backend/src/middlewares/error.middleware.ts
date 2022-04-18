f0a4e9a15a3512f3f5cf1f40bd8ef24cc2f9042d
Omar <omar@kamelizer.com>
2022-04-18 20:20:37 +0200

    add error middleware

---
+++ qurba-backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { MongooseError } from "mongoose";
+
+const error = (err: Error, req: Request, res: Response, next: NextFunction) => {
+	let statusCode: number = 500;
+	let error: Error = err;
+
+	if (process.env.NODE_ENV === "development") {
+		console.log({ err });
+	}
+
+	if (err instanceof SyntaxError) {
+		statusCode = 400;
+	}
+
+	else if (err instanceof MongooseError) {
+		statusCode = 422;
+	}
+
+	res.status(statusCode).json({
+		code: statusCode,
+		error: error.message
+	});
+};
+
+export default error;
