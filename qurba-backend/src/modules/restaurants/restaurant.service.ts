e1622cefe229d758e160eac8e474f3421b82d085
Omar <omar@kamelizer.com>
2022-04-18 22:19:05 +0200

    populate cuisine for restaurant by id

--- qurba-backend/src/modules/restaurants/restaurant.service.ts
+++ qurba-backend/src/modules/restaurants/restaurant.service.ts
@@ -1,7 +1,15 @@
+import { Document } from "mongoose";
 import BaseService from "../base/base.service";
 
+
 export default class RestaurantService extends BaseService {
+	restaurantModel: any;
 	constructor(model: any) {
 		super(model)
+		this.restaurantModel = model;
+	}
+
+	async findById(id: string): Promise<Document[]> {
+		return this.model.findById(id).populate(["cuisine"]).exec()
 	}
 }
