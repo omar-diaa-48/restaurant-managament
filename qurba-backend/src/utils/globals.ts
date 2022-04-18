e96f63e8f8e6368b808a16be5627d4bf0fc46d94
Omar <omar@kamelizer.com>
2022-04-18 23:47:26 +0200

    add global object

---
+++ qurba-backend/src/utils/globals.ts
@@ -0,0 +1,24 @@
+export const GLOBALS = {
+	ACTIONS: {
+		POST: {
+			title: "POST",
+			code: 201
+		},
+		GET: {
+			title: "GET",
+			code: 200
+		},
+		PUT: {
+			title: "PUT",
+			code: 200
+		},
+		PATCH: {
+			title: "PATCH",
+			code: 200
+		},
+		DELETE: {
+			title: "DELETE",
+			code: 200
+		},
+	}
+}
