29cbe5c2092df3694591bb7b0baa27e42bc22e4b
Omar <omar@kamelizer.com>
2022-04-18 20:20:26 +0200

    add server mount routes and handle error

--- qurba-backend/src/server.ts
+++ qurba-backend/src/server.ts
@@ -1,5 +1,10 @@
 import cors from "cors";
 import express from "express";
+import * as swaggerUi from 'swagger-ui-express';
+import error from "./middlewares/error.middleware";
+
+const swaggerDocument = require('./swagger.json');
+
 
 // create a server class
 class Server {
@@ -9,11 +14,38 @@ class Server {
 		this.app = express();
 
 		this.configureMiddlewares();
+		this.mountRoutes();
 	}
 
 	private configureMiddlewares() {
+		//parse all requests middleware
 		this.app.use(express.json())
+		//allow cors
 		this.app.use(cors())
+	}
+
+	private mountRoutes(): void {
+		//use api-docs for swagger documentation of APIs
+		this.app.use(
+			'/api-docs',
+			swaggerUi.serve,
+			swaggerUi.setup(swaggerDocument, {
+				swaggerOptions: {
+					persistAuthorization: true
+				}
+			})
+		);
+
+		// use if the route is not found
+		this.app.use('*', (req, res) => {
+			res.status(404).json({
+				message: 'API_NOT_FOUND',
+				code: 404
+			});
+		});
+
+		// use to handle application errors
+		this.app.use(error);
 	}
 }
 
