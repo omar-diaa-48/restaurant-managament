9856253480e1772be93bb784fac16b27597ae59e
Omar <omar@kamelizer.com>
2022-04-19 14:25:54 +0200

    adjust user routes

--- qurba-backend/src/modules/users/user.routes.ts
+++ qurba-backend/src/modules/users/user.routes.ts
@@ -5,7 +5,11 @@ import UserController from "./user.controller";
 const userRouter: Router = Router();
 const userController: UserController = new UserController(User);
 
-userRouter.post('/', userController.addOne)
+userRouter.route('/')
+	.get(userController.listAll)
+	.post(userController.addOne)
+
+userRouter.get('/:id', userController.findById)
 
 userRouter.post('/:id/restaurants', userController.addRestaurant)
 
